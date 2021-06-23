6879b7803e96985ea1f11ced1618d6e0adb923ae
sunho <tnsgh4711@naver.com>
2021-06-23 12:20:20 +0900

    Signed-off-by: sunho <tnsgh4711@naver.com>

---
+++ ITS_BD_API/connDB.py
@@ -0,0 +1,43 @@
+import cx_Oracle
+import pyodbc
+import os
+from pyhive import hive
+
+
+# dsname is used only in tibero.
+# dbhost, dbport is not used in tibero
+# hive don't use dbuser, dbpwd
+def get_conn(dbtype, dsname, dbuser, dbpwd, dbhost, dbport):
+    if dbtype == "hive":
+        conn = hive.Connection(host=dbhost, port=dbport, auth="NOSASL", database="default")
+        cur = conn.cursor()
+
+        return cur
+
+    elif dbtype == "oracle":
+        os.chdir(r'instantclient_19_11')
+        os.putenv('NLS_LANG', 'AMERICAN_AMERICA.UTF8')
+
+        db = cx_Oracle.connect(user=dbuser, password=dbpwd, dsn=dbhost)
+        cur = db.cursor()
+
+        return cur
+
+    elif dbtype == "tibero":
+
+        cur = pyodbc.connect('DSN=' + dsname + ';UID=' + dbuser + ';PWD=' + dbpwd)
+        # cur.setdecoding(pyodbc.SQL_CHAR, encoding='utf-8')
+        # cur.setdecoding(pyodbc.SQL_WCHAR, encoding='utf-8')
+        # cur.setdecoding(pyodbc.SQL_WMETADATA, encoding='utf-8')
+        cur.setencoding(encoding='utf-8')
+
+        return cur
+    #
+    # elif dbtype == "MSSQL":
+    #
+    #
+    # elif dbtype == "MYSQL":
+    #
+    #
+    # elif dbtype == "MARIADB":
+
