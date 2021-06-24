b0a0a0f92fa19aa02842de9e1c3800d8e1a9a48a
sunho <tnsgh4711@naver.com>
2021-06-24 11:16:10 +0900

    connection

--- dbconn/connDB.py
+++ dbconn/connDB.py
@@ -1,18 +1,21 @@
 import cx_Oracle
 import pyodbc
+import pymysql
+import mariadb
+import pymssql
 import os
 from pyhive import hive
 
 
-# dsname is used only in tibero.
+# dsname is used only in tibero, mariadb.
 # dbhost, dbport is not used in tibero
 # hive don't use dbuser, dbpwd
+
 def get_conn(dbtype, dsname, dbuser, dbpwd, dbhost, dbport):
+    cur = None
     if dbtype == "hive":
         conn = hive.Connection(host=dbhost, port=dbport, auth="NOSASL", database="default")
         cur = conn.cursor()
-
-        return cur
 
     elif dbtype == "oracle":
         os.chdir(r'instantclient_19_11')
@@ -20,13 +23,6 @@ def get_conn(dbtype, dsname, dbuser, dbpwd, dbhost, dbport):
 
         db = cx_Oracle.connect(user=dbuser, password=dbpwd, dsn=dbhost)
         cur = db.cursor()
-
-##push test aa
-
-        ##push test
-        ##push test
-
-        return cur
 
     elif dbtype == "tibero":
 
@@ -36,13 +32,17 @@ def get_conn(dbtype, dsname, dbuser, dbpwd, dbhost, dbport):
         # cur.setdecoding(pyodbc.SQL_WMETADATA, encoding='utf-8')
         cur.setencoding(encoding='utf-8')
 
-        return cur
-    #
-    # elif dbtype == "MSSQL":
-    #
-    #
-    # elif dbtype == "MYSQL":
-    #
-    #
-    # elif dbtype == "MARIADB":
+    elif dbtype == "mssql":
+        conn = pymssql.connect(host=dbhost, database=dsname, port=dbport, charset='utf8')
+        cur = conn.cursor()
+
+    elif dbtype == "mysql":
+        conn = pymysql.connect(host=dbhost, user=dbuser, password=dbpwd, port=int(dbport), db=dsname, charset='utf8')
+        cur = conn.cursor()
+
+    elif dbtype == "mariadb":
+        conn = mariadb.connect(host=dbhost, user=dbuser, password=dbpwd, port=int(dbport), database=dsname)
+        cur = conn.cursor()
+
+    return cur
 
--- polls/views.py
+++ polls/views.py
@@ -3,55 +3,42 @@ import json
 from django.shortcuts import render
 from django.http import HttpResponse
 from pyhive import hive
-import cx_Oracle
-import pyodbc
-import os
+from dbconn.connDB import get_conn
+
 # Create your views here.
-
-
 def get_table(request):
-    #오라클 연결방법
-    # os.chdir(r'C:\instantclient_19_11')
-    # os.putenv('NLS_LANG', 'AMERICAN_AMERICA.UTF8')
-    #
-    #
-    # db = cx_Oracle.connect(user="GW_SCFF", password="GWSU12#$", dsn="112.216.254.226:1521/CMELU")
-    # cursor = db.cursor()
-    #
-    # cursor.execute("SELECT * FROM USER_OBJECTS WHERE OBJECT_TYPE='TABLE'")
-    # a = cursor.fetchall()
-    # cursor.close()
-    # db.close()
 
     # UPMS(티베로), BIG_DATA(하이브)
     # POST로 받은 값이 null이면 GET으로 받음
     body_unicode = request.body.decode('utf-8')
-    body = json.loads(body_unicode)
-    tspace = body['tspace']
-    if tspace is None:
+    if len(body_unicode) != 0:
+        body = json.loads(body_unicode)
+        tspace = body['tspace']
+
+    else:
         tspace = request.GET.get("tspace")
 
+    # get_conn(dbtype, dsname, dbuser, dbpwd, dbhost, dbport):
+    # dbhost, dbport is not used in tibero. because tibero is connected of odbc
+    # hive don't use dbuser, dbpwd
     if tspace == "BIG_DATA":
-        host = "192.168.0.133"
-        port = 10000
-        conn = hive.Connection(host=host, port=port, auth="NOSASL", database="default")
-        cur = conn.cursor()
+        cur = get_conn("hive", "", "", "", "192.168.0.133", 10000)
         cur.execute("show tables")
 
     elif tspace == "UPMS":
-        # 티베로
-        DSNNAME = 'TIBERO'
-        DBUSER = 'UPMS'
-        DBPWD = 'UPMS12#$'
+        cur = get_conn("tibero", "TIBERO", "UPMS", "UPMS12#$", "", "")
 
-        conn = pyodbc.connect('DSN=' + DSNNAME + ';UID=' + DBUSER + ';PWD=' + DBPWD)
-        # conn.setdecoding(pyodbc.SQL_CHAR, encoding='utf-8')
-        # conn.setdecoding(pyodbc.SQL_WCHAR, encoding='utf-8')
-        # conn.setdecoding(pyodbc.SQL_WMETADATA, encoding='utf-8')
-        conn.setencoding(encoding='utf-8')
+        cur = cur.execute(
+            "select A.TABLE_NAME TABLE_NAME, A.OWNER TABLESPACE_NAME, B.COMMENTS COMMENTS, A.NUM_ROWS NUM_ROWS from ALL_TABLES A, ALL_TAB_COMMENTS B where A.TABLE_NAME = B.TABLE_NAME AND A.OWNER = '" + tspace + "' " +
+            "UNION ALL SELECT A.VIEW_NAME TABLE_NAME, A.OWNER TABLESPACE_NAME, B.COMMENTS COMMENTS, 0 NUM_ROWS from ALL_VIEWS A, USER_TAB_COMMENTS B where A.VIEW_NAME = B.TABLE_NAME AND A.OWNER='" + tspace + "' ORDER BY TABLE_NAME")
 
-        cur = conn.execute("select A.TABLE_NAME TABLE_NAME, A.OWNER TABLESPACE_NAME, B.COMMENTS COMMENTS, A.NUM_ROWS NUM_ROWS from ALL_TABLES A, ALL_TAB_COMMENTS B where A.TABLE_NAME = B.TABLE_NAME AND A.OWNER = '"+tspace+"' "+
-			"UNION ALL SELECT A.VIEW_NAME TABLE_NAME, A.OWNER TABLESPACE_NAME, B.COMMENTS COMMENTS, 0 NUM_ROWS from ALL_VIEWS A, USER_TAB_COMMENTS B where A.VIEW_NAME = B.TABLE_NAME AND A.OWNER='"+tspace+"' ORDER BY TABLE_NAME")
+    elif tspace == "MARIADB":
+        cur = get_conn("mariadb", "silla_dorm", "dorm", "mapsco1338", "dev.mapsco.kr", "3309")
+        cur.execute("show tables")
+
+    elif tspace == "MYSQL":
+        cur = get_conn("mysql", "sys", "root", "0000", "localhost", "3309")
+        cur.execute("show tables")
 
     else:
         return HttpResponse("No tspace")
@@ -77,7 +64,10 @@ def get_table(request):
 
         array_dict.append(tables)
         json_array = json.dumps(array_dict)
-    return HttpResponse(json_array,  content_type="application/json; charset=utf-8")
+
+    cur.close()
+    return HttpResponse(json_array, content_type="application/json; charset=utf-8")
+
 
 def get_column(request):
     tspace = request.args.get("tspace")
