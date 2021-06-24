b487458924d0d8b0f1a7e486ef9b6c4f45cf2e4e
hslee <hslee@swinnus.com>
2021-06-24 14:21:48 +0900

    함수 추가

--- polls/urls.py
+++ polls/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('getTable', views.get_table, name='get_table'),
     path('getColumn', views.get_column, name='get_columns'),
+    path('insertTable', views.insertTable, name='insertTable'),
 ]
