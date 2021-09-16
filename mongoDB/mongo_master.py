4a704b31539d026f8ec94d1186bb786a06fcf390
hslee <hslee@swinnus.com>
2021-09-16 18:27:07 +0900

    2021-09-01

--- mongoDB/mongo_master.py
+++ mongoDB/mongo_master.py
@@ -5,6 +5,38 @@ from django.http import HttpResponse
 mongo_host = "192.168.0.100"
 mongo_port = 27017
 
+# 몽고디비 collection 생성
+# host : mongodb 주소
+# port : mongodb 포트
+# dbs : mongodb database
+# collection : mongodb collection
+def createCollection(request):
+    body_unicode = request.body.decode('utf-8')
+    if len(body_unicode) != 0:
+        body = json.loads(body_unicode)
+        dbs = body['db']
+        collection = body['collection']
+    else:
+        collection = request.GET.get("collection")
+        dbs = request.GET.get("dbs")
+
+
+    mongoconn = MongoClient(mongo_host, int(mongo_port))
+    database = mongoconn[dbs]
+    try:
+        # database내에 있는 Collection목록을 가지옴
+        collectionList = database.list_collection_names()
+        # Collection 존재 여부 체크
+        if collection in collectionList:
+            return HttpResponse(json.dumps({'DB_YN': 'N'}), 200)
+        else:
+            newcol = database.create_collection(collection)
+            print(database.list_collection_names())
+            return HttpResponse(json.dumps({'DB_YN':'Y'}), 200)
+    except:
+        resp = HttpResponse(status=400)
+        return resp
+
 # 성과지표 몽고디비 collection 생성
 # host : mongodb 주소
 # port : mongodb 포트
--- mongoDB/urls.py
+++ mongoDB/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import mongo_master as monggo
 
 urlpatterns = [
+    path('createCollection', monggo.createCollection, name="createCollection"),
     path('createIRCollection', monggo.createIRCollection, name="createIRCollection")
 ]
